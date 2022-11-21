eab416fff7864376c188f798c33c4b42883ed48a
smilekritik <kostia.ponomarenko@gmail.com>
2022-11-21 17:31:58 +0200

    cleared old work

--- WeddingTestingLab/WeddingTests/UnitTest.cs
+++ WeddingTestingLab/WeddingTests/UnitTest.cs
@@ -9,27 +9,6 @@ namespace WeddingTests
     {
 
 
-        [TestMethod]
-        public void CheckChengesClothes()
-        {
-            order Some = new order();
-            bool actual = Some.AddCloth("test", 322);
-            bool expected = true;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void CheckSummary()
-        {
-            order Some = new order();
-            Some.AddCloth("test", 322);
-            Some.AddCar("test2", 10);
-            double expected = 322 + 10;
-            double actual = Some.Summary();
-
-            Assert.AreEqual(expected, actual);
-        }
 
     }
 }
