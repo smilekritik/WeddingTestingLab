b98f806848a8c5f48c7af214336ea3c3132c28a8
smilekritik <kostia.ponomarenko@gmail.com>
2022-11-21 20:30:42 +0200

    Added another tests (same as prev)

--- WeddingTestingLab/WeddingTestingLab/Program.cs
+++ WeddingTestingLab/WeddingTestingLab/Program.cs
@@ -15,6 +15,8 @@ namespace WeddingTestingLab
         }
 
         public List<cars> car { get; set; } = new List<cars>();
+        public List<clothes> cloth { get; set; } = new List<clothes>();
+        public List<ceremonies> ceremonie { get; set; } = new List<ceremonies>();
 
         public bool AddCar(string Name, double Price)
         {
@@ -31,5 +33,25 @@ namespace WeddingTestingLab
             var test = car.Single(r => r.name == Name);
             return car.Remove(test);
         }
+
+        public bool AddCloth(string Name, double Price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveCloth(string Name, double Price)
+        {
+            throw new NotImplementedException();
+        }
+        public bool AddCeremonie(string Place, double Price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveCeremonie(string Place, double Price)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- WeddingTestingLab/WeddingTests/UnitTest.cs
+++ WeddingTestingLab/WeddingTests/UnitTest.cs
@@ -45,5 +45,81 @@ namespace WeddingTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CheckGetCloth()
+        {
+            List<clothes> test = new List<clothes>();
+            clothes temp = new clothes("some", 1);
+            test.Add(temp);
+            var actual = test;
+
+            order Some = new order();
+            Some.AddCloth("some", 1);
+            var expected = Some.car;
+
+            var expectedjson = JsonConvert.SerializeObject(expected);
+            var actualjson = JsonConvert.SerializeObject(actual);
+            Assert.AreEqual(expectedjson, actualjson);
+        }
+
+        [TestMethod]
+        public void CheckAddCloth()
+        {
+            order Some = new order();
+            bool actual = Some.AddCloth("test", 20);
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckRemoveCloth()
+        {
+            order Some = new order();
+            Some.AddCloth("test", 20);
+            bool actual = Some.RemoveCloth("test", 20);
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckGetCeremonie()
+        {
+            List<ceremonies> test = new List<ceremonies>();
+            ceremonies temp = new ceremonies("some", 1);
+            test.Add(temp);
+            var actual = test;
+
+            order Some = new order();
+            Some.AddCeremonie("some", 1);
+            var expected = Some.car;
+
+            var expectedjson = JsonConvert.SerializeObject(expected);
+            var actualjson = JsonConvert.SerializeObject(actual);
+            Assert.AreEqual(expectedjson, actualjson);
+        }
+
+        [TestMethod]
+        public void CheckAddCeremonie()
+        {
+            order Some = new order();
+            bool actual = Some.AddCeremonie("test", 20);
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckRemoveCeremonie()
+        {
+            order Some = new order();
+            Some.AddCeremonie("test", 20);
+            bool actual = Some.RemoveCeremonie("test", 20);
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
